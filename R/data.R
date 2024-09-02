ae5269b0a856c0fa51e4afdcfd677f36df306838
Luke McGuinness <luke.a.mcguiness@gmail.com>
2024-09-02 18:37:07 +0100

    Reorder columns in example datasets, and clean up documentation for these datasets

--- R/data.R
+++ R/data.R
@@ -1,86 +1,19 @@
-#' @title DATASET_TITLE
-#' @description DATASET_DESCRIPTION
-#' @format A data frame with 20 rows and 26 variables:
-#' \describe{
-#'   \item{\code{result_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{study}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{type}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{yi}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{vi}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{d1j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d2j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d3j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d4j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d5j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d6j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d7j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d1t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d2t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d3t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d4t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d5t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d6t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d7t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d1d}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d2d}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d3d}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d4d}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d5d}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d6d}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d7d}}{character COLUMN_DESCRIPTION}
-#'}
-#' @details DETAILS
+#' @title Example risk of bias assessments
+#' @format A data frame with 20 rows and 26 variables
 "dat_bias"
 
-#' @title DATASET_TITLE
-#' @description DATASET_DESCRIPTION
-#' @format A data frame with 3 rows and 6 variables:
-#' \describe{
-#'   \item{\code{domain}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{bias_m_add}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{bias_v_add}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{bias_m_prop}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{bias_v_prop}}{double COLUMN_DESCRIPTION}
-#'}
-#' @details DETAILS
+#' @title Example priors for the bias adjustment
+#' @format A data frame with 3 rows and 6 variables
 "dat_bias_values"
 
 
-#' @title DATASET_TITLE
-#' @description DATASET_DESCRIPTION
-#' @format A data frame with 20 rows and 14 variables:
-#' \describe{
-#'   \item{\code{result_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{study}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{type}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{yi}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{vi}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{d1j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d2j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d3j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d1t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d2t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d3t}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d1d}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d2d}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{d3d}}{character COLUMN_DESCRIPTION}
-#'}
-#' @details DETAILS
+#' @title Example indirectness assessments
+#' @format A data frame with 20 rows and 14 variables
 "dat_ind"
 
-#' @title DATASET_TITLE
-#' @description DATASET_DESCRIPTION
-#' @format A data frame with 3 rows and 6 variables:
-#' \describe{
-#'   \item{\code{domain}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{j}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{ind_m_add}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{ind_v_add}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{ind_m_prop}}{double COLUMN_DESCRIPTION}
-#'   \item{\code{ind_v_prop}}{double COLUMN_DESCRIPTION}
-#'}
-#' @details DETAILS
+#' @title Example priors for indirectness adjustment
+#' @format A data frame with 3 rows and 6 variables
+
 "dat_ind_values"
 
 
