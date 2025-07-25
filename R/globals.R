4591d885b688601f01ca29361d6f2960ddafd5a1
tassiajones <nv22679@bristol.ac.uk>
2025-07-25 13:05:54 +0100

    added sensitivity analysis vignettes
    added how to analyse how sensitive results are to prior beliefs and plot them, also did an interactive shiny plot so users can see how their priors affect results easily and did a vignette for this. also added me and chin yang as authors

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,5 @@
 utils::globalVariables(c(
   "Study", "Weights", "author", "cneg", "cpos", "heading", "judgement",
   "n", "offset", "overall", "par", "rma", "rob_dummy", "rob_tools",
-  "row_n", "tneg", "tpos", "year"
+  "row_n", "tneg", "tpos", "year", "x", "se", "est_type", "result_id"
 ))
